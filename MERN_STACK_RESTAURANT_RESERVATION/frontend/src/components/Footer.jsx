66c3544b8eae2e77a254586ea6af254a4b3d664a
Amit_kumar <kumaramit.nnp@gmail.com>
2025-06-25 11:48:27 +0530

    added to repo

---
+++ MERN_STACK_RESTAURANT_RESERVATION/frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">AMIT</div>
+          <div className="right">
+            <p>REVA CIRCLE , BENGALURU</p>
+            <p>Open: 07:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By AMIT</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By AMIT(AV).</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
